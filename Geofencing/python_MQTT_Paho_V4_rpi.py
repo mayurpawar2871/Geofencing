ba29a907e5ed8c43e6b74ba5e2894b0e5407dcea
mayurpawar2871 <mayurpawar2871@gmail.com>
2020-03-07 11:55:52 +0900

    Deleted old & freezing working version

--- Geofencing/python_MQTT_Paho_V4_rpi.py
+++ Geofencing/python_MQTT_Paho_V4_rpi.py
@@ -1,18 +1,30 @@
 #MQTT Working with broker mqtt.eclipse.org instead of iot.eclipse.org
 #JSON decoder reads MQTT msg from broker after subscribing 
 #dictionary m_in["batt"] checks for battery level 32 & print status 
+# 31 oct , timlong is to move RPI
+# If lattitude long is 35.717 then using import dec , trim funtion extracted only 717 value
+# Feb 9 COMPLETED ,Working with ON/OFF FAN when leaves from home & come at home
+
 
 import paho.mqtt.client as mqtt #import the client1
 import time
 import json
-
+import os
+import subprocess
+import decimal
 
 brokers_out={"broker2":"mqtt.eclipse.org"}
 print(brokers_out)
-print("brokers_out is a ",type(brokers_out))
-print("broker 2 address = ",brokers_out["broker2"])
+#print("brokers_out is a ",type(brokers_out))
+#print("broker 2 address = ",brokers_out["broker2"])
 
 
+def trim_func(variable):
+    temp = int(variable)
+    variable =variable - temp
+    variable =variable*1000
+    variable =int(variable)
+    return variable;
 
 ############
 def on_message(client, userdata, msg):
@@ -24,15 +36,28 @@ def on_message(client, userdata, msg):
     m_decode=str(msg.payload.decode("utf-8","ignore"))
     #print("message received " ,str(message.payload.decode("utf-8")))
     #print("data Received type",type(m_decode))
-    print("data Received",m_decode)
-    print("Converting from Json to Object")
+    #print("data Received",m_decode)
+    #print("Converting from Json to Object")
     m_in=json.loads(m_decode) #decode json data
     #print(type(m_in))
     print("ACTUAL DATA = ",m_in["batt"])
-    if m_in["batt"] == 32:
-     print("BATTERY FULL")
+    batt= m_in["batt"]
+    Lattitude=m_in["lat"]
+    Lattitude=35.71723
+    Lattitude=trim_func(Lattitude);
+    Longitude=m_in["lon"]
+    #Longitude=139.96923
+    Longitude=trim_func(Longitude);
+    print("Battery=",batt)
+    print("Lattitude",Lattitude)
+    print("Longitude",Longitude) 
+    if (Lattitude == 717 and  
+       Longitude == 969 or Longitude == 968 ):
+     print("YOU ARE AT HOME")
+     os.system('sudo ./uhubctl -l 1-1 -p 2 -a on ')  
     else:
-     print("BATTERY EMPTY")
+     print("NOT AT HOME")
+     os.system('sudo ./uhubctl -l 1-1 -p 2 -a off ')
 ########################################
 #  format '{"_type":"location","acc":15,"alt":77,"batt":39,"conn":"w","inregions":["home"],"lat":35.7175005,"lon":139.9688588,"t":"u","tid":"ge","tst":1569174818,"vac":2,"vel":0}')
 
@@ -47,7 +72,7 @@ i = 1
 while True:
  client.loop_start() #start the loop
 
- print("Subscribing to topic","owntracks/geofence/id1")
+ #print("Subscribing to topic","owntracks/geofence/id1")
  client.subscribe("owntracks/geofence/id1") 
 
  time.sleep(4) # wait
---
+++ Geofencing/test_trim.py
@@ -0,0 +1,13 @@
+import json
+import decimal
+
+
+variable= 35.7172425
+
+temp = int(variable)
+variable =variable - temp
+variable =variable*1000
+variable =int(variable)
+
+print("variable=",variable)
+
